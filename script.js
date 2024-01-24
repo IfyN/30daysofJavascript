491d6ff3a1762d953df387c61e181f5fbced4240
IfyN <ifynwosu0@gmail.com>
2024-01-24 21:07:40 +0100

    uppdate image margin and border size according to spacing slider input

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let blurSlider = document.getElementById("blur");
-let baseSlider = document.getElementById("base");
+let spaceSlider = document.getElementById("spacing");
 let image = document.getElementById("image");
 
 blurSlider.addEventListener("input", slideToBlur);
+spaceSlider.addEventListener("input", slideToSpace);
 
 function slideToBlur() {
   // get slider current value
   const blurValue = event.target.value;
   //update intensity of the blur based on the value of the slider
   image.style.filter = `blur(${blurValue}px)`;
+}
+
+function slideToSpace() {
+  const spaceValue = event.target.value;
+  image.style.margin = `${spaceValue}px`;
+  image.style.border = `${spaceValue}px solid red`;
 }
